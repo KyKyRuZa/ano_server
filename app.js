9f1ffc134de5cb938af90e2e705a023e9f798780
KyKyRuZa <ituhvatullin8@gmail.com>
2025-11-28 19:48:28 +0300

    upd 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const { authLimiter, apiLimiter, strictLimiter } = require('./middleware/rateLimit');
+// const { authLimiter, apiLimiter, strictLimiter } = require('./middleware/rateLimit');
 
 const AuthRoute = require('./routes/AuthRoute');
 const StaffRoute = require('./routes/StaffRoute');
@@ -14,13 +14,11 @@ const { logger, morganStream } = require('./logger');
 
 const app = express();
 
-// Morgan middleware
 const morganMiddleware = morgan((tokens, req, res) => {
     const date = new Date().toISOString().replace('T', ' ').split('.')[0];
     return `[${date}] ${tokens.method(req, res)} ${tokens.url(req, res)} ${tokens.status(req, res)} ${tokens['response-time'](req, res)} ms`;
 }, { stream: morganStream });
 
-// Middleware
 app.use(cors({
     origin: ['http://localhost:3001', 'https://anotsenimzhizn.ru'],
     credentials: true,
@@ -30,8 +28,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morganMiddleware);
 
-app.use('/api/auth/login'); 
-app.use('/api/'); 
+// app.use('/api/auth/login'); 
+// app.use('/api/'); 
 
 app.use('/uploads', express.static('/var/www/uploads'));
 
