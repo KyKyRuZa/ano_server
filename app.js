e674b89cd4c1260227455506b16069f8db516528
KyKyRuZa <ituhvatullin8@gmail.com>
2026-01-07 15:53:13 +0300

    add

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
 dotenv.config();
 
 const AuthRoute = require('./routes/AuthRoute');
